fe171bf562caeaf6f2be36626d3de0796e13929b
RomanReznichenko <roma.reznichenko@smartexe.com>
2023-08-30 11:22:50 +0300

    fix: provide ErorStateMatcher in root

--- projects/core/src/lib/errors/error-state-matcher.ts
+++ projects/core/src/lib/errors/error-state-matcher.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { AbstractControl, FormGroupDirective, NgForm } from '@angular/forms';
 
 /** Provider that defines how form controls behave with regards to displaying error messages. */
-@Injectable()
+@Injectable({ providedIn: 'root' })
 export class ErrorStateMatcher {
     isErrorState(control: AbstractControl | null, form: FormGroupDirective | NgForm | null): boolean {
         return !!(control && control.invalid && (control.touched || (form && form.submitted)));
