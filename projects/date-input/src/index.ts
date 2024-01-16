8bea5fac7f4bf4602c2b6835882b74b12623c741
RomanReznichenko <roma.reznichenko@smartexe.com>
2024-01-16 13:49:24 +0200

    fix: fix pakages build

--- projects/date-input/src/index.ts
+++ projects/date-input/src/index.ts
@@ -1,1 +1,3 @@
 export * from './lib/date-input.module';
+export * from './lib/date-input.component';
+export * from './lib/date-field.directive';
--- projects/drawer/src/index.ts
+++ projects/drawer/src/index.ts
@@ -1,1 +1,4 @@
 export * from './lib/drawer.module';
+export * from './lib/drawer.component';
+export * from './lib/content/content.component';
+export * from './lib/container/drawer-container.component';
