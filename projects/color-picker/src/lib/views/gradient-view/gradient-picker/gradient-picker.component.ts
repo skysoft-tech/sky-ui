99243663087694a76dc5f840226a35702f469bbe
RomanReznichenko <roma.reznichenko@smartexe.com>
2024-01-30 11:18:06 +0200

    feat(color-picker): rework gradient picker

--- projects/color-picker/src/lib/views/gradient-view/gradient-picker/gradient-picker.component.ts
+++ projects/color-picker/src/lib/views/gradient-view/gradient-picker/gradient-picker.component.ts
@@ -9,9 +9,11 @@ import {
     ViewChild,
     ViewEncapsulation,
     SimpleChanges,
+    ElementRef,
+    AfterViewInit,
 } from '@angular/core';
 import { SkyTriangleTrackerDirective } from './triangle-tracker.directive';
-import { ColorConverter, Hex, Rgb, SkyPosition, skyClamp } from '@sky-ui/core';
+import { ColorAnalyzer, ColorComparer, ColorConverter, Hex, Rgb } from '@sky-ui/core';
 
 @Component({
     selector: 'sky-gradient-picker',
@@ -23,10 +25,10 @@ import { ColorConverter, Hex, Rgb, SkyPosition, skyClamp } from '@sky-ui/core';
         class: 'sky-gradient-picker',
     },
 })
-export class SkyGradientPickerComponent implements OnInit, OnChanges {
+export class SkyGradientPickerComponent implements OnInit, AfterViewInit, OnChanges {
     hue: number = 0;
-    value: number = 0;
     saturation: number = 0;
+    value: number = 0;
 
     @Input()
     color!: Rgb;
@@ -35,40 +37,44 @@ export class SkyGradientPickerComponent implements OnInit, OnChanges {
         return ColorConverter.rgb2hex(this.color);
     }
 
-    get pureColor(): Hex {
+    get puusreColor(): Hex {
         return ColorConverter.hsv2hex([this.hue, 1, 1]);
     }
 
-    positionTest: SkyPosition = { x: 0, y: 0 };
+    get selectorBorder(): Hex {
+        const color = ColorAnalyzer.pickReadableColor(this.color);
+        return ColorConverter.rgb2hex(color);
+    }
 
     @Output()
     colorChange: EventEmitter<Rgb> = new EventEmitter<Rgb>();
 
     @ViewChild(SkyTriangleTrackerDirective) triangle?: SkyTriangleTrackerDirective;
+    @ViewChild('canvas') canvas?: ElementRef<HTMLCanvasElement>;
+    @ViewChild('origin') origin?: ElementRef<HTMLElement>;
 
     ngOnInit(): void {
         const [h, s, v] = ColorConverter.rgb2hsv(this.color);
         this.hue = h;
         this.value = v;
         this.saturation = s;
+    }
 
-        this.positionTest = {
-            x: this.saturation,
-            y: this.value === 1 ? this.value * 0.5 : this.value,
-        };
+    ngAfterViewInit(): void {
+        this.drawTriangle(this.puusreColor);
     }
 
     ngOnChanges(changes: SimpleChanges): void {
-        if (changes['color']) {
+        if (changes['color'] && !changes['color'].firstChange) {
             const [h, s, v] = ColorConverter.rgb2hsv(this.color);
-            this.hue = h;
-            this.value = v;
-            this.saturation = s;
-            // this.cdr.detectChanges();
-            this.positionTest = {
-                x: this.saturation,
-                y: skyClamp(this.saturation - this.value, 0, 1),
-            };
+            const prevColor = ColorConverter.hsv2rgb([this.hue, this.saturation, this.value]);
+            if (!ColorComparer.compare(prevColor, this.color)) {
+                this.hue = h;
+                this.value = v;
+                this.saturation = s;
+            }
+
+            this.drawTriangle(this.puusreColor);
         }
     }
 
@@ -77,17 +83,69 @@ export class SkyGradientPickerComponent implements OnInit, OnChanges {
         this.notifyAboutChange();
     }
 
-    saturationValueChange(position: SkyPosition): void {
-        const { x, y } = position;
-        this.saturation = x;
-        this.value = skyClamp(y + x, 0, 1);
+    saturationValueChange([saturation, value]: [number, number]): void {
+        this.saturation = saturation;
+        this.value = value;
 
         this.notifyAboutChange();
     }
 
     private notifyAboutChange(): void {
         const rgb = ColorConverter.hsv2rgb([this.hue, this.saturation, this.value]);
-        this.color = rgb;
         this.colorChange.emit(rgb);
+    }
+
+    private drawTriangle(color: Hex): void {
+        const ctx = this.canvas?.nativeElement.getContext('2d');
+
+        if (!ctx) {
+            return;
+        }
+        const { width, height } = this.origin!.nativeElement.getBoundingClientRect();
+        ctx.clearRect(0, 0, width, height);
+        const a = { x: 0, y: 0 };
+        const b = { x: width, y: height / 2 };
+        const c = { x: 0, y: height };
+
+        ctx.beginPath();
+
+        ctx.moveTo(a.x, a.y);
+        ctx.lineTo(b.x, b.y);
+        ctx.lineTo(c.x, c.y);
+        ctx.closePath();
+
+        ctx.strokeStyle = '#d8d0d0bf';
+        ctx.lineWidth = 1;
+        ctx.stroke();
+        ctx.fillStyle = 'transparent';
+        ctx.fill();
+
+        // set blend mode
+        ctx.globalCompositeOperation = 'color-burn';
+        const radius = 140;
+
+        const grd1 = ctx.createRadialGradient(a.x, a.y, 0, a.x, a.y, radius);
+        grd1.addColorStop(0, '#000000FF');
+        grd1.addColorStop(1, '#00000000');
+
+        const grd2 = ctx.createRadialGradient(b.x, b.y, 0, b.x, b.y, radius);
+        grd2.addColorStop(0, color + 'FF');
+        grd2.addColorStop(1, color + '00');
+
+        const grd3 = ctx.createRadialGradient(c.x, c.y, 0, c.x, c.y, radius);
+        grd3.addColorStop(0, '#FFFFFFFF');
+        grd3.addColorStop(1, '#FFFFFF00');
+
+        ctx.fillStyle = grd1;
+        ctx.fill();
+
+        ctx.fillStyle = grd2;
+        ctx.fill();
+
+        ctx.fillStyle = grd3;
+        ctx.fill();
+
+        // if you need to draw something else, don't forget to reset the gCO
+        ctx.globalCompositeOperation = 'source-over';
     }
 }
--- projects/color-picker/src/lib/views/gradient-view/gradient-picker/triangle-tracker.directive.ts
+++ projects/color-picker/src/lib/views/gradient-view/gradient-picker/triangle-tracker.directive.ts
@@ -12,7 +12,10 @@ import {
     SimpleChanges,
     SkipSelf,
 } from '@angular/core';
-import { SkyDestroyService, SkyMouseTracker, SkyPosition, skyClamp } from '@sky-ui/core';
+import { SkyDestroyService, SkyMouseTracker, SkyPosition } from '@sky-ui/core';
+
+type Triangle = [SkyPosition, SkyPosition, SkyPosition];
+type Vector = [number, number];
 
 @Directive({
     selector: '[skyTriangleTracker]',
@@ -29,12 +32,14 @@ export class SkyTriangleTrackerDirective extends SkyMouseTracker implements Afte
     origin!: HTMLElement;
 
     @Input()
-    initialPosition: SkyPosition = { x: 0, y: 0 };
+    value: [number, number] = [0, 0];
+
+    @Output()
+    valueChange: EventEmitter<[number, number]> = new EventEmitter<[number, number]>();
 
     cursorPosition!: SkyPosition;
 
-    @Output()
-    valueChange: EventEmitter<SkyPosition> = new EventEmitter<SkyPosition>();
+    private originalTriangle!: Triangle;
 
     constructor(
         @SkipSelf() private cdr: ChangeDetectorRef,
@@ -44,13 +49,30 @@ export class SkyTriangleTrackerDirective extends SkyMouseTracker implements Afte
     ) {
         super(elementRef, documentRef, destroy);
         this.trackEvent.subscribe((p: SkyPosition) => {
-            const rect = this.origin.getBoundingClientRect();
-            const position = super.getPosition(p.x, p.y, rect);
-            this.cursorPosition = this.getCurrentPosition(position, rect);
-            this.valueChange.emit({
-                x: this.cursorPosition.x / rect.width,
-                y: this.cursorPosition.y / rect.height,
-            });
+            const [a, b, c] = this.originalTriangle;
+            let point: SkyPosition | null = null;
+            const inTriangle = this.isInTriangle(p, this.originalTriangle);
+
+            if (inTriangle) {
+                this.emitValueChange(p);
+                return;
+            }
+
+            if (this.isInSector(p, c, a, b)) {
+                point = this.getPointProjection(p, c, b);
+            }
+
+            if (this.isInSector(p, a, b, c)) {
+                point = this.getPointProjection(p, a, c);
+            }
+
+            if (this.isInSector(p, b, c, a)) {
+                point = this.getPointProjection(p, a, b);
+            }
+
+            if (point) {
+                this.emitValueChange(point);
+            }
         });
     }
 
@@ -63,58 +85,157 @@ export class SkyTriangleTrackerDirective extends SkyMouseTracker implements Afte
             this.parentContainer = this.elementRef.nativeElement.parentElement!;
         }
 
-        // const rect = this.origin.getBoundingClientRect();
-        // const position = {
-        //     x: rect.width * this.initialPosition.x,
-        //     y: rect.height * (this.initialPosition.y === 1 ? 0.5 : this.initialPosition.y),
-        // };
+        const { width, height } = this.origin.getBoundingClientRect();
+        const a = { x: 0, y: 0 };
+        const b = { x: width, y: height / 2 };
+        const c = { x: 0, y: height };
+        this.originalTriangle = [a, b, c];
 
-        // this.cursorPosition = this.getCurrentPosition(position, rect);
-        // this.cdr.detectChanges();
+        const [s, v] = this.value;
+        const saturation = s * height;
+        const value = v * width;
+        this.cursorPosition = this.getTrianglePosition(saturation, value);
+
+        this.cdr.detectChanges();
     }
 
     ngOnChanges(changes: SimpleChanges): void {
-        // TODO: fix initial position changing
-        // if (changes['initialPosition']) {
-        //     const rect = this.origin.getBoundingClientRect();
-        //     const position = {
-        //         x: rect.width * (1 / (this.initialPosition.x + this.initialPosition.y)) * this.initialPosition.x,
-        //         y: rect.height * (1 / (this.initialPosition.x + this.initialPosition.y)) * this.initialPosition.y,
-        //     };
-        //     this.cursorPosition = this.getCurrentPosition(position, rect);
-        // }
+        if (changes['value'] && !changes['value'].firstChange) {
+            const { width, height } = this.origin.getBoundingClientRect();
+            const [s, v] = this.value;
+            const saturation = s * height;
+            const value = v * width;
+            this.cursorPosition = this.getTrianglePosition(saturation, value);
+            this.cdr.detectChanges();
+        }
     }
 
-    protected override getPosition(x: number, y: number, { left, top, width, height }: DOMRect): SkyPosition {
-        const parentRect = this.parentContainer.getBoundingClientRect();
-        const position = super.getPosition(x, y, parentRect);
-        const rotatedPoint = this.rotate(-this.angle, position);
-        rotatedPoint.x = rotatedPoint.x + parentRect.left;
-        rotatedPoint.y = rotatedPoint.y + parentRect.top;
+    protected override getPosition(x: number, y: number, _: DOMRect): SkyPosition {
+        const { top, left } = this.origin.getBoundingClientRect();
+        const { top: pTop, left: pLeft, width: pWidth, height: pHeight } = this.parentContainer.getBoundingClientRect();
+        const origin = {
+            x: pWidth / 2 + pLeft,
+            y: pHeight / 2 + pTop,
+        };
 
-        return rotatedPoint;
+        const rotatedPoint = this.rotate({ x, y }, -this.angle, origin);
+
+        return {
+            x: rotatedPoint.x - left,
+            y: rotatedPoint.y - top,
+        };
     }
 
-    private rotate(angle: number, position: SkyPosition): SkyPosition {
-        const parentRect = this.parentContainer.getBoundingClientRect();
-        const center = { x: parentRect.width / 2, y: parentRect.height / 2 };
+    protected override shouldTrack(event: MouseEvent, rect: DOMRect): boolean {
+        const p = this.getPosition(event.clientX, event.clientY, rect);
+        return this.isInTriangle(p, this.originalTriangle);
+    }
+
+    // https://blackpawn.com/texts/pointinpoly/default.html#:~:text=Barycentric%20Technique
+    private isInTriangle(p: SkyPosition, [a, b, c]: Triangle): boolean {
+        const v0 = this.calculateVector(c, a);
+        const v1 = this.calculateVector(b, a);
+        const v2 = this.calculateVector(p, a);
+
+        const dot00 = this.dotProduct(v0, v0);
+        const dot01 = this.dotProduct(v0, v1);
+        const dot02 = this.dotProduct(v0, v2);
+        const dot11 = this.dotProduct(v1, v1);
+        const dot12 = this.dotProduct(v1, v2);
+
+        const invDenom = 1 / (dot00 * dot11 - dot01 * dot01);
+
+        const u = (dot11 * dot02 - dot01 * dot12) * invDenom;
+        const v = (dot00 * dot12 - dot01 * dot02) * invDenom;
+
+        return u >= 0 && v >= 0 && u + v < 1;
+    }
+
+    // https://stackoverflow.com/a/13675772
+    private isInSector(p: SkyPosition, a: SkyPosition, b: SkyPosition, c: SkyPosition): boolean {
+        const ba = this.calculateVector(b, a);
+        const bc = this.calculateVector(b, c);
+        const bp = this.calculateVector(b, p);
+
+        return !this.areClockwise(bc, bp) && this.areClockwise(ba, bp);
+    }
+
+    private calculateVector(p1: SkyPosition, p2: SkyPosition): Vector {
+        return [p2.x - p1.x, p2.y - p1.y];
+    }
+
+    private dotProduct([x1, y1]: Vector, [x2, y2]: Vector): number {
+        return x1 * x2 + y1 * y2;
+    }
+
+    private areClockwise([x1, y1]: Vector, [x2, y2]: Vector): boolean {
+        return -x1 * y2 + y1 * x2 > 0;
+    }
+
+    // http://www.sunshine2k.de/coding/java/PointOnLine/PointOnLine.html
+    private getPointProjection(p: SkyPosition, a: SkyPosition, b: SkyPosition): SkyPosition | null {
+        const ab = this.calculateVector(a, b);
+        const ap = this.calculateVector(a, p);
+
+        const area = this.dotProduct(ab, ab);
+        const val = this.dotProduct(ab, ap);
+        const len = ab[0] * ab[0] + ab[1] * ab[1];
+
+        if (val < 0) {
+            return a;
+        }
+
+        if (val > area) {
+            return b;
+        }
+
+        return {
+            x: a.x + (val * ab[0]) / len,
+            y: a.y + (val * ab[1]) / len,
+        };
+    }
+
+    private rotate(position: SkyPosition, angle: number, origin: SkyPosition): SkyPosition {
         const radians = angle * (Math.PI / 180);
         const cos = Math.cos(radians);
         const sin = Math.sin(radians);
 
-        const normalX = center.x - position.x;
-        const normalY = center.y - position.y;
+        const normalX = position.x - origin.x;
+        const normalY = position.y - origin.y;
 
         const newX = normalX * cos - normalY * sin;
         const newY = normalX * sin + normalY * cos;
 
-        return { x: center.x - newX, y: center.y - newY };
+        return { x: newX + origin.x, y: newY + origin.y };
     }
 
-    private getCurrentPosition({ x, y }: SkyPosition, rect: DOMRect): SkyPosition {
-        const a = y < rect.height / 2 ? y : rect.height - y;
-        const xMax = Math.sqrt(Math.pow(a * 2, 2) - Math.pow(a, 2));
-        x = skyClamp(x, 0, xMax);
+    private getTrianglePosition(s: number, v: number): SkyPosition {
+        const { height, width } = this.origin.getBoundingClientRect();
+        const sqrt3 = Math.sqrt(3);
+
+        const x = (s * v * height * sqrt3) / (2 * height * width);
+        const y = (v * height * sqrt3 - x * width) / (width * sqrt3);
+
         return { x, y };
+    }
+
+    private getDataFromPositio({ x, y }: SkyPosition): [number, number] {
+        const { height, width } = this.origin.getBoundingClientRect();
+        const sqrt3 = Math.sqrt(3);
+
+        const s = y + x / sqrt3 === 0 ? 0 : (x * 2 * height) / (y * sqrt3 + x);
+        const v = ((y + x / sqrt3) * width) / height;
+
+        return [s, v];
+    }
+
+    private emitValueChange(p: SkyPosition): void {
+        const { height, width } = this.origin.getBoundingClientRect();
+        const [saturation, value] = this.getDataFromPositio(p);
+
+        const s = saturation / height;
+        const v = value / width;
+
+        this.valueChange.emit([s, v]);
     }
 }
