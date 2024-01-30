e457a21dfbf1fa343e811b2777f144ff142cf32b
RomanReznichenko <roma.reznichenko@smartexe.com>
2024-01-30 10:14:51 +0200

    fix(core): add missed exports

--- projects/core/src/lib/tools/color/index.ts
+++ projects/core/src/lib/tools/color/index.ts
@@ -2,3 +2,5 @@ export { Hex, Hsv, Rgb, Rgba, RgbString, RgbaString } from './types';
 export { ColorMixer } from './color-mixer';
 export { ColorConverter } from './color-converter';
 export { ColorValidator } from './color-validator';
+export { ColorAnalyzer } from './color-analyzer';
+export { ColorComparer } from './color-comparer';
