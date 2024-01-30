93706cf21bc42340c384323bde6d5dadd89b3e15
RomanReznichenko <roma.reznichenko@smartexe.com>
2024-01-30 10:15:41 +0200

    fix(core): fix hex validator

--- projects/core/src/lib/tools/color/color-validator.ts
+++ projects/core/src/lib/tools/color/color-validator.ts
@@ -2,7 +2,7 @@ import { Hex } from './types';
 
 export class ColorValidator {
     public static isHexValid(value: Hex): boolean {
-        const regexp = RegExp('^#[a-f0-9]*$');
+        const regexp = RegExp('^#[A-Fa-f0-9]*$');
         if (!regexp.test(value)) {
             return false;
         }
